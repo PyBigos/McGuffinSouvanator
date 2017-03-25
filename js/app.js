623287a0599a5b1cc2f3f889d3a650e493ec941c
Jeff Bigos <jbigos@esri.com>
2017-03-25 13:53:59 -0400

    domby adds

--- js/app.js
+++ js/app.js
@@ -18,9 +18,9 @@ require([
     console.log("load");
 
     var map = new Map("map", {
-        basemap: "streets-night-vector",
-        center: [-122.304568, 47.608492],
-        zoom: 13
+        basemap: "gray",
+        center: [-71.097312, 42.346567],
+        zoom: 18
     });
 
     var hits = new FeatureLayer("https://services7.arcgis.com/Gk8wYdLBgQPxqVZU/arcgis/rest/services/RedSox/FeatureServer/0",{
@@ -29,5 +29,32 @@ require([
     });
 
     map.addLayer(hits);
+
+
+    //Inherit from a webmap
+
+    //Find the options submitted
+    //on(dom.byId("btnLocate"), "click", doAddressToLocations);
+
+    var park = dom.byId("park").value;
+    var team = dom.byId("team").value;
+    var dTime = dom.byId("dTime").value;
+    var inning = dom.byId("inning").value;
+
+    //Build query Object
+    var query = new Query();
+
+
+
+    //Pitch this to the graph
+    hits.on("query-features-complete",function(queryRes){
+
+        console.log(queryRes.features.length);
+
+
+    });
+
+
+
 
 });
