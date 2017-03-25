8ae3693cced7d8634cff9bd67882bb05bd79b2b1
Jeff Bigos <jbigos@esri.com>
2017-03-25 13:34:48 -0400

    first wire

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
-/**
- * Created by jeff2832 on 3/25/2017.
- */
+require([
+    "esri/map",
+    "esri/layers/FeatureLayer",
+    "esri/tasks/query",
+    "esri/tasks/StatisticDefinition",
+    "esri/geometry/geometryEngine",
+    "esri/symbols/SimpleMarkerSymbol",
+    "esri/symbols/SimpleLineSymbol",
+    "esri/symbols/SimpleFillSymbol",
+    "esri/graphic",
+    "esri/Color",
+    "dojo/dom",
+    "dojo/domReady!"
+], function(Map, FeatureLayer, Query, StatisticDefinition, geometryEngine,
+            SimpleMarkerSymbol, SimpleLineSymbol, SimpleFillSymbol, Graphic, Color, dom) {
+
+
+    console.log("load");
+
+    var map = new Map("map", {
+        basemap: "streets-night-vector",
+        center: [-122.304568, 47.608492],
+        zoom: 13
+    });
+
+    var hits = new FeatureLayer("https://services7.arcgis.com/Gk8wYdLBgQPxqVZU/arcgis/rest/services/RedSox/FeatureServer/0",{
+
+        outFields:["*"]
+    });
+
+    map.addLayer(hits);
+
+});
